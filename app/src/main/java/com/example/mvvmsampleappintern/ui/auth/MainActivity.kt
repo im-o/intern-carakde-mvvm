e27ff132320faf79d83cdde07ec47fc872055e25
im-o <frizgt@gmail.com>
2020-08-28 18:30:11 +0800

    project started

---
+++ app/src/main/java/com/example/mvvmsampleappintern/ui/auth/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mvvmsampleappintern.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvmsampleappintern.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
