cf66efa19f41f402856f6f2e2133f5ab2c1c7542
im-o <frizgt@gmail.com>
2020-09-03 17:49:45 +0800

    :zap: #18 Android MVVM Architecture Tutorial - activeNetworkInfo Deprecated

--- app/src/main/java/com/example/mvvmsampleappintern/data/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/com/example/mvvmsampleappintern/data/network/NetworkConnectionInterceptor.kt
@@ -2,6 +2,7 @@ package com.example.mvvmsampleappintern.data.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import com.example.mvvmsampleappintern.utils.NoInternetException
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -23,9 +24,17 @@ class NetworkConnectionInterceptor(
         return chain.proceed(chain.request())
     }
     private fun isInternetAvailable() : Boolean {
+        var result = false
         val connectivityManager = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        connectivityManager.activeNetworkInfo.also {
-            return it != null && it.isConnected
+        connectivityManager.let {
+            it.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
+                result = when {
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    else -> false
+                }
+            }
         }
+        return result
     }
 }
