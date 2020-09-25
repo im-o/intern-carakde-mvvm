77081f989fa269af28e6198bd9c987e40875f61e
im-o <frizgt@gmail.com>
2020-09-25 15:14:59 +0800

    :zap: #2 WorkManager - Sending/Receiving Data

--- app/src/main/java/com/example/mvvmsampleappintern/ui/workmanager/WorkRequestActivity.kt
+++ app/src/main/java/com/example/mvvmsampleappintern/ui/workmanager/WorkRequestActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import com.example.mvvmsampleappintern.R
@@ -11,21 +12,43 @@ import com.example.mvvmsampleappintern.databinding.ActivityWorkRequestBinding
 import com.example.mvvmsampleappintern.workers.MyWorker
 
 class WorkRequestActivity : AppCompatActivity() {
+    companion object {
+        const val KEY_TASK_DESC = "key_task_desc"
+    }
+
     private lateinit var binding: ActivityWorkRequestBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_work_request)
+        initWorkerManger()
+    }
 
-        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java).build()
+    private fun initWorkerManger() {
+        val data = Data.Builder()
+            .putString(KEY_TASK_DESC, "Hey I am sending the work data from activity")
+            .build()
+
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .build()
+
         binding.workStartedMB.setOnClickListener {
             WorkManager.getInstance(this).enqueue(oneTimeWorkRequest)
         }
 
         WorkManager.getInstance(this).getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
             .observe(this, Observer {
+                if (it != null) {
+                    if (it.state.isFinished) {
+                        val data1 = it.outputData
+                        val output = data1.getString(MyWorker.KEY_TASK_OUTPUT)
+                        binding.workerResultTV.append("out : $output \n")
+                    }
+                }
+
                 val status = it.state.name
-                binding.workerResultTV.append("$status \n")
+                binding.workerResultTV.append("status : $status \n")
             })
     }
 }
--- app/src/main/java/com/example/mvvmsampleappintern/workers/MyWorker.kt
+++ app/src/main/java/com/example/mvvmsampleappintern/workers/MyWorker.kt
@@ -8,28 +8,39 @@ import android.media.RingtoneManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.mvvmsampleappintern.R
+import com.example.mvvmsampleappintern.ui.workmanager.WorkRequestActivity
 
 /**
  * Created by rivaldy on Sep/24/2020.
  * Find me on my lol Github :D -> https://github.com/im-o
  */
 
-class MyWorker(ctx: Context, params: WorkerParameters): Worker(ctx, params) {
+class MyWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
     companion object {
         const val NOTIFICATION_ID = 1
         const val CHANNEL_ID = "channel_01"
         const val CHANNEL_NAME = "work_manager_channel"
+        const val KEY_TASK_OUTPUT = "key_task_output"
     }
 
     override fun doWork(): Result {
-        showNotification("Testing worker","Work is finished, yey!!")
-        return Result.success()
+        val data = inputData
+        val desc = data.getString(WorkRequestActivity.KEY_TASK_DESC)
+        showNotification("Hey I am your worker", desc)
+
+        val data1 = Data.Builder()
+            .putString(KEY_TASK_OUTPUT, "From Worker -> Task Finish Successfully")
+            .build()
+
+        return Result.success(data1)
     }
+
     private fun showNotification(title: String?, body: String?) {
-        val vibrate = longArrayOf(300,300,300,300,300)
+        val vibrate = longArrayOf(300, 300, 300, 300, 300)
         val notificationManagerCompat = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
@@ -41,7 +52,7 @@ class MyWorker(ctx: Context, params: WorkerParameters): Worker(ctx, params) {
             .setDefaults(NotificationCompat.DEFAULT_ALL)
             .setSound(alarmSound)
             .setAutoCancel(true)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
             channel.enableVibration(true)
             channel.vibrationPattern = vibrate
